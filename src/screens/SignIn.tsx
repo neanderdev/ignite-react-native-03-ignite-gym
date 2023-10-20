c18d733570aaed063a3adb14e1406c874ca1aff0
Neander de Souza <neander.faria@hotmail.com>
2023-10-20 08:44:38 -0300

    Component Library - Componente Heading

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
@@ -19,6 +19,12 @@ export function SignIn() {
                 <Text color="gray.100" fontSize="sm">
                     Treine sua mente e o seu corpo.
                 </Text>
+            </Center>
+
+            <Center>
+                <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+                    Acesse a conta
+                </Heading>
             </Center>
         </VStack>
     );

cb015e47ff52e0c32c21cb643e6d8d5fc659c95b
Neander de Souza <neander.faria@hotmail.com>
2023-10-20 11:30:52 -0300

    Finalizando as Interfaces - Listando exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,8 +6,9 @@ import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
-    const [groupSelected, setGroupSelected] = useState('Costas');
     const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro']);
+    const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
+    const [groupSelected, setGroupSelected] = useState('Costas');
 
     return (
         <VStack flex={1}>
@@ -39,13 +40,21 @@ export function Home() {
                     </Heading>
 
                     <Text color="gray.200" fontSize="sm">
-                        4
+                        {exercises.length}
                     </Text>
                 </HStack>
 
-                <ExerciseCard />
-
-                <ExerciseCard />
+                <FlatList
+                    data={exercises}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <ExerciseCard />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{
+                        paddingBottom: 20
+                    }}
+                />
             </VStack>
         </VStack>
     );

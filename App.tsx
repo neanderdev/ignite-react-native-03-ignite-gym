5e79f0fb50250a513f5ddda92926d56131ceb7bc
Neander de Souza <neander.faria@hotmail.com>
2023-10-20 08:34:18 -0300

    Component Library - Instalando o NativeBase

--- App.tsx
+++ App.tsx
@@ -1,22 +1,19 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { StatusBar, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { StatusBar, View } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      {
-        fontsLoaded
-          ? <Text>Hello World</Text>
-          : <View />
-      }
-    </View>
+      {fontsLoaded ? <View /> : <View />}
+    </NativeBaseProvider>
   );
 }

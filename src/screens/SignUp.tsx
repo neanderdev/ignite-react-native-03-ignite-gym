4655ddf3a7b51eb22d987e26620494ec038317a4
Neander de Souza <neander.faria@hotmail.com>
2023-10-20 13:31:43 -0300

    Formulários - Estilizando mensagens de validação

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -63,11 +63,10 @@ export function SignUp() {
                                 placeholder="Nome"
                                 onChangeText={onChange}
                                 value={value}
+                                errorMessage={errors.name?.message}
                             />
                         )}
                     />
-
-                    <Text color="white">{errors.name?.message}</Text>
 
                     <Controller
                         control={control}
@@ -86,11 +85,10 @@ export function SignUp() {
                                 autoCapitalize="none"
                                 onChangeText={onChange}
                                 value={value}
+                                errorMessage={errors.email?.message}
                             />
                         )}
                     />
-
-                    <Text color="white">{errors.email?.message}</Text>
 
                     <Controller
                         control={control}

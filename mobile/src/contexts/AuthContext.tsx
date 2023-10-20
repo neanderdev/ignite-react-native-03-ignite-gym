1bf03667e9e3d15af54b8ac5cd2bc2df365c5c98
Neander de Souza <neander.faria@hotmail.com>
2023-10-20 14:30:08 -0300

    Contextos no React Native - Buscando dados do usuário no back-end

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,10 +1,12 @@
 import { createContext, ReactNode, useState } from "react";
 
+import { api } from '@services/api';
+
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
     user: UserDTO;
-    singIn: (email: string, password: string) => void;
+    singIn: (email: string, password: string) => Promise<void>;
 }
 
 type AuthContextProviderProps = {
@@ -14,20 +16,18 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
-    const [user, setUser] = useState({
-        id: '1',
-        name: 'Neander de Souza',
-        email: 'neanderdev@email.com',
-        avatar: 'neanderdev.png'
-    })
+    const [user, setUser] = useState<UserDTO>({} as UserDTO)
 
-    function singIn(email: string, password: string) {
-        setUser({
-            id: '',
-            name: '',
-            email,
-            avatar: '',
-        })
+    async function singIn(email: string, password: string) {
+        try {
+            const { data } = await api.post('/sessions', { email, password });
+
+            if (data.user) {
+                setUser(data.user);
+            }
+        } catch (error) {
+            throw error;
+        }
     }
 
     return (

d69ff6069e0413af810aac45498e7cacc81f88dc
Neander de Souza <neander.faria@hotmail.com>
2023-10-20 11:19:18 -0300

    Finalizando as Interfaces - Criando componente Group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,14 @@
 import { VStack } from 'native-base';
 
+import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 
 export function Home() {
     return (
         <VStack flex={1}>
             <HomeHeader />
+
+            <Group name="Costas" />
         </VStack>
     );
 }

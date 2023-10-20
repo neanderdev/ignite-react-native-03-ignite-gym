aa31d6507e1c1b8f26738bd75100170bbbf507bb
Neander de Souza <neander.faria@hotmail.com>
2023-10-20 13:12:59 -0300

    Galeria de Fotos - Utilizando mensagens toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import * as FileSystem from 'expo-file-system';
 import * as ImagePicker from 'expo-image-picker';
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from 'native-base';
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 
@@ -14,6 +14,8 @@ const PHOTO_SIZE = 33;
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
     const [userPhoto, setUserPhoto] = useState('https://github.com/neanderdev.png');
+
+    const toast = useToast();
 
     async function handleUserPhotoSelected() {
         setPhotoIsLoading(true);
@@ -34,7 +36,11 @@ export function Profile() {
                 const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
 
                 if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
-                    return Alert.alert('Essa imagem é muito grande. Escolha uma de até 5MB.');
+                    return toast.show({
+                        title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+                        placement: 'top',
+                        bgColor: 'red.500'
+                    })
                 }
 
                 setUserPhoto(photoSelected.assets[0].uri);

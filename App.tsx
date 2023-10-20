2f830c3cf9c92970319936ab475d2ff9b68d4fcd
Neander de Souza <neander.faria@hotmail.com>
2023-10-20 08:38:23 -0300

    Component Library - Customizando o tema default

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { StatusBar } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
 import { Loading } from '@components/Loading';
+
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      <Loading />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
